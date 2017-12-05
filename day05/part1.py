31ab512b465a85ade6dd8b2bea0c67dc00178358
Elliot J. Voris <elliot@voris.me>
2017-12-05 09:40:52 -0600

    Day 05 Revisited
    - Tried to clean up a bit, make it a little more concise, too
    - Put the hard work into a function
    - Seems to run faster, too! Part 2 runs in about half the time!

--- day05/part1.py
+++ day05/part1.py
@@ -1,14 +1,19 @@
-my_input = open('day05\input.txt', 'rt')
 my_maze = []
-for l in my_input:
-    my_maze.append(int(l.strip()))
-print(my_maze)
-my_steps = 0
-my_index = 0
-next_index = 0
-while my_index < len(my_maze):
-    my_steps += 1
-    next_index = my_index + my_maze[my_index]
-    my_maze[my_index] += 1
-    my_index = next_index
-print(my_steps)
+
+with open('day05\input.txt', 'rt') as file:
+    for line in file:
+        my_maze.append(int(line.strip()))
+file.close()
+
+def FindStepsOutOfMaze(maze):
+    my_steps = my_index = next_index = 0
+
+    while my_index < len(maze):
+        my_steps += 1
+        next_index = my_index + maze[my_index]
+        maze[my_index] += 1
+        my_index = next_index
+
+    return my_steps
+
+print(FindStepsOutOfMaze(my_maze))
